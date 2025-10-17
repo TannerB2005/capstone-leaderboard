e80d816f5f84376235a75ea65b99a770d4fbf4ba
Matthew Curtis <160465720+mdcurt@users.noreply.github.com>
2025-10-17 15:00:55 -0500

    Refactor ShipmentsBarChart component to simplify chartType definition and clean up options computation

--- src/components/charts/shipments-bar-chart/shipments-bar-chart.component.ts
+++ src/components/charts/shipments-bar-chart/shipments-bar-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GoogleChartsModule } from 'angular-google-charts';
+import { GoogleChartsModule, ChartType } from 'angular-google-charts';
 import { ScorecardStore } from '../../../stores/scorecard.store';
 
 @Component({
@@ -12,16 +12,12 @@ import { ScorecardStore } from '../../../stores/scorecard.store';
 })
 export class ShipmentsBarChartComponent {
   constructor(private store: ScorecardStore) {}
-  chartType = computed(() => this.store.selectedCarrierId() == null ? 'ColumnChart' : 'ColumnChart');
+  chartType = ChartType.ColumnChart;
   data = computed(() => this.store.shipmentsSeries());
-  options = computed(() => {
-    const selected = this.store.selectedCarrierId() != null;
-    return {
-      legend: { position: 'none' },
-      hAxis: { title: selected ? 'Week' : 'Carrier' },
-      vAxis: { title: 'Shipments' },
-      colors: ['#7c3aed'],
-      chartArea: { left: 60, right: 20, top: 24, bottom: 48, width: '100%', height: '70%' }
-    };
-  });
+  options = computed(() => ({
+    legend: { position: 'none' },
+    hAxis: { title: this.store.selectedCarrierId() == null ? 'Carrier' : 'Week' },
+    vAxis: { title: 'Shipments' },
+    chartArea: { left: 60, right: 20, top: 24, bottom: 48, width: '100%', height: '70%' }
+  }));
 }
